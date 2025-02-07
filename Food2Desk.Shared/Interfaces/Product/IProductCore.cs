f0f5ce9d9fb598e5b2cd6fcf6e726de511694947
juhgadotti <jgadotti27@gmail.com>
2025-02-06 21:55:47 -0300

    injetando interfaces

--- Food2Desk.Shared/Interfaces/Product/IProductCore.cs
+++ Food2Desk.Shared/Interfaces/Product/IProductCore.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Food2Desk.Shared.Model;
+using Food2Desk.Shared.DTOs;
 
-namespace Food2Desk.Core.CoreInterfaces
+namespace Food2Desk.Shared.Interfaces   
 {
     public interface IProductCore
     {
-        public List<ProductModel> List();
+        public List<ProductDTO> List();
     }
 }
--- Food2Desk/Program.cs
+++ Food2Desk/Program.cs
@@ -1,3 +1,9 @@
+using Food2Desk.Core;
+using Food2Desk.Shared.Interfaces;
+using Food2Desk.DataAccess.DataAccess;
+using Food2Desk.Shared.Interfaces.Product;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
@@ -11,6 +17,8 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
+builder.Services.AddScoped<IProductCore, Product>();
+builder.Services.AddScoped<IProductDataAccess, ProductDataAccess>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
