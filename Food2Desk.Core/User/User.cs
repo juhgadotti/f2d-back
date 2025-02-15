766d190d05c974c5d417a7d739031d5ed203a9d3
juhgadotti <jgadotti27@gmail.com>
2025-02-14 22:22:38 -0300

    criação do user core e das interfaces

---
+++ Food2Desk.Core/User/User.cs
@@ -0,0 +1,32 @@
+﻿using Food2Desk.Shared.Interfaces.User;
+using Food2Desk.Shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food2Desk.Core.User
+{
+    public class User : IUserCore
+    {
+        public UserModel Get()
+        {
+            OfficeModel office = new OfficeModel()
+            {
+                Floor = "11",
+                Number = "102",
+                Enterprise = "Numax",
+                EnterpriseId = 3
+            };
+            UserModel user = new UserModel()
+            {
+                Name = "Joao",
+                Id = Guid.NewGuid(),
+                Offices = new List<OfficeModel>() { office }
+            };
+
+            return user;
+        }
+    }
+}
---
+++ Food2Desk.Shared/Interfaces/User/IUserDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food2Desk.Shared.Interfaces.User
+{
+    public interface IUserDataAccess
+    {
+    }
+}
