9e748ac1ae4f1a974ba2232bc308f541d0f6fd27
juhgadotti <jgadotti27@gmail.com>
2025-03-05 23:15:54 -0300

    model do order

---
+++ Food2Desk.Shared/Model/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Food2Desk.Shared.Model
+{
+    public class OrderModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public Guid UserId { get; set; }
+        public decimal TotalCharge { get; set; }
+        public bool DeliveryNow { get; set; }
+        public string? DeliveryTime { get; set; }
+        public OfficeModel Office { get; set; }
+        public List<ProductModel> Cart { get; set; }
+    }
+}
