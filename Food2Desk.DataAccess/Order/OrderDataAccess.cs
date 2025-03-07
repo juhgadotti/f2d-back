f73719bb9d1336413150fe9e6738378bcbc2c692
juhgadotti <jgadotti27@gmail.com>
2025-03-06 23:42:03 -0300

    create: orderdataacces + interface

---
+++ Food2Desk.DataAccess/Order/OrderDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Food2Desk.Shared.Interfaces.Order;
+
+namespace Food2Desk.DataAccess.Order
+{
+    public class OrderDataAccess : IOrderDataAccess
+    {
+    }
+}
--- Food2Desk/Program.cs
+++ Food2Desk/Program.cs
@@ -4,6 +4,7 @@ using Food2Desk.DataAccess.User;
 using Food2Desk.DataAccess.Product;
 using Food2Desk.Shared.Interfaces.Product;
 using Food2Desk.Shared.Interfaces.User;
+using Food2Desk.Shared.Interfaces.Order;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,7 @@ builder.Services.AddCors(options =>
 // Add services to the container.
 builder.Services.AddScoped<IProductCore, Product>();
 builder.Services.AddScoped<IProductDataAccess, ProductDataAccess>();
+builder.Services.AddScoped<IOrderDataAccess, OrderDataAccess>();
 builder.Services.AddScoped<IUserCore, User>();
 builder.Services.AddScoped<IUserDataAccess, UserDataAccess>();
 
