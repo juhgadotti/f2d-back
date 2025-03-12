4a5f5a8873a322ffcfb9d36a58f9105ed40adde1
Juliana Gadotti <juliana@numax.com.br>
2025-03-12 15:26:22 -0300

    correção de erros de build

--- Food2Desk.Shared/DTOs/ProductDTO.cs
+++ Food2Desk.Shared/DTOs/ProductDTO.cs
@@ -8,11 +8,11 @@ namespace Food2Desk.Shared.DTOs
 {
     public class ProductDTO
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string? ImageUrl { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public double Price {  get; set; }
-        public int Category { get; set; }
+        public int? Category { get; set; }
     }
 }
