cb9d1c31a3324d30280b9c63030ccc0054352a29
juhgadotti <jgadotti27@gmail.com>
2025-03-16 23:23:17 -0300

    ajuste

--- Food2Desk/Controllers/OrderController.cs
+++ Food2Desk/Controllers/OrderController.cs
@@ -24,7 +24,7 @@ namespace Food2Desk.Controllers
         [HttpGet("list")]
         public IActionResult List()
         {
-            return Ok();
+            return Ok(); //commit de doming
         }
 
         [HttpPost("")]
