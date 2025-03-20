7cff4d49e195384e342f1939fbadb3cebbecec45
juhgadotti <jgadotti27@gmail.com>
2025-03-19 22:39:38 -0300

    fix: cart.price in list() orderDA

--- Food2Desk.DataAccess/Order/OrderDataAccess.cs
+++ Food2Desk.DataAccess/Order/OrderDataAccess.cs
@@ -4,24 +4,33 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Food2Desk.Shared.DTOs;
 using Food2Desk.Shared.Interfaces.Order;
+using Food2Desk.Shared.Interfaces.Product;
 using Food2Desk.Shared.Model;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Food2Desk.DataAccess.Order
-{
+{   
     public class OrderDataAccess : IOrderDataAccess
     {
+        private readonly IProductDataAccess ProductDA;
+        public OrderDataAccess(IProductDataAccess productDA)
+        {
+            ProductDA = productDA;
+        }
+
         public OrderModel Insert(OrderModel model)
         {
             model.UserName = "deu certinho";
 
             return model;
-        }
-       
+        }       
 
         public List<OrderModel> List()
         {
+            List<ProductDTO> productList = ProductDA.List();
+
             var orders = new List<OrderModel>
             {
                 new OrderModel
@@ -34,8 +43,8 @@ namespace Food2Desk.DataAccess.Order
                     Office = new OfficeModel { OfficeId = Guid.NewGuid(), Floor = "12", Number = "331" },
                     Cart = new List<ProductModel>
                     {
-                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-95F5E2E79A6E"), Name = "Coxinha", Price = 7.99M, Quantity = 2 },
-                        new ProductModel { Id = Guid.Parse("76B62969-25CE-4FBC-BE96-19B2447C69E7"), Name = "Coca cola", Price = 5.99M, Quantity = 1 }
+                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-95F5E2E79A6E"), Name = "Coxinha", Price = 7.99, Quantity = 2 },
+                        new ProductModel { Id = Guid.Parse("76B62969-25CE-4FBC-BE96-19B2447C69E7"), Name = "Coca cola", Price = 5.99, Quantity = 1 }
                     },
                     TotalCharge = (2 * 7.99M) + (1 * 5.99M)
                 },
@@ -49,8 +58,8 @@ namespace Food2Desk.DataAccess.Order
                     Office = new OfficeModel { OfficeId = Guid.NewGuid(), Floor = "12", Number = "321" },
                     Cart = new List<ProductModel>
                     {
-                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-93G1E2E79A6E"), Name = "Pastel", Price = 10.00M, Quantity = 1 },
-                        new ProductModel { Id = Guid.Parse("64794FA6-31A2-4E33-8E3C-3D8FE8A57827"), Name = "Agua", Price = 2.99M, Quantity = 2 }
+                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-93G1E2E79A6E"), Name = "Pastel", Price = 10.00, Quantity = 1 },
+                        new ProductModel { Id = Guid.Parse("64794FA6-31A2-4E33-8E3C-3D8FE8A57827"), Name = "Agua", Price = 2.9, Quantity = 2 }
                     },
                     TotalCharge = (1 * 10.00M) + (2 * 2.99M)
                 },
@@ -64,9 +73,9 @@ namespace Food2Desk.DataAccess.Order
                     Office = new OfficeModel { OfficeId = Guid.NewGuid(), Floor = "1", Number = "31" },
                     Cart = new List<ProductModel>
                     {
-                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-95F5E2E79A6E"), Name = "Coxinha", Price = 7.99M, Quantity = 3 },
-                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-93G1E2E79A6E"), Name = "Pastel", Price = 10.00M, Quantity = 2 },
-                        new ProductModel { Id = Guid.Parse("76B62969-25CE-4FBC-BE96-19B2447C69E7"), Name = "Coca cola", Price = 5.99M, Quantity = 1 }
+                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-95F5E2E79A6E"), Name = "Coxinha", Price = 7.99, Quantity = 3 },
+                        new ProductModel { Id = Guid.Parse("8CFB6326-8E5E-4BFE-B51E-93G1E2E79A6E"), Name = "Pastel", Price = 10.00, Quantity = 2 },
+                        new ProductModel { Id = Guid.Parse("76B62969-25CE-4FBC-BE96-19B2447C69E7"), Name = "Coca cola", Price = 5.99, Quantity = 1 }
                     },
                     TotalCharge = (3 * 7.99M) + (2 * 10.00M) + (1 * 5.99M)
                 }
