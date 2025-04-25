24e6f77c76b190930f11a2839812dacb7bf76091
juhgadotti <jgadotti27@gmail.com>
2025-04-24 23:26:12 -0300

    crate: lunch model

---
+++ Food2Desk.Shared/Model/LunchModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food2Desk.Shared.Model
+{
+    public class LunchModel
+    {
+        public Guid Id { get; set; }
+        public string WeekDay { get; set; }
+        public int WeekDayId { get; set; }
+        public List<ProductModel> Lunchs { get; set; }
+
+    }
+}
