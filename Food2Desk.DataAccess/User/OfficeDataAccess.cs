13f048ead836282cb19fba132ea7f4238d2981ac
juhgadotti <jgadotti27@gmail.com>
2025-05-27 21:47:02 -0300

    office da + interface

---
+++ Food2Desk.DataAccess/User/OfficeDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using Food2Desk.DataAccess.Context;
+using Food2Desk.Shared.DTOs;
+using Food2Desk.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food2Desk.DataAccess
+{
+    public class OfficeDataAccess(Food2deskContext context) : BaseRepository<OfficeDTO>(context), IOfficeDataAccess
+    {
+        private readonly Food2deskContext DBContext = context;        
+    }
+}
---
+++ Food2Desk.Shared/Interfaces/User/IOfficeDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using Food2Desk.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food2Desk.Shared.Interfaces
+{
+    public interface IOfficeDataAccess : IBaseRepository<OfficeDTO>
+    {
+    }
+}
